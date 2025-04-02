8e1a7872e7a8f21ff287a578f0dc7f8af9cb8373
Martin Jarábek <martin.jar91@seznam.cz>
2025-04-02 17:06:24 +0200

    změna adresy ve views.py a nastavení BASE_DIR v settings

--- main/views.py
+++ main/views.py
@@ -7,4 +7,4 @@ def home(request):
         'leetcode_link': 'odkaz_na_leetcode',
         'github_link': 'https://github.com/tvoje-jmeno',
     }
-    return render(request, 'main/index.html', context)
+    return render(request, 'index.html', context)
