5b9adf4a4b1581458a6da74c9541ac8aa76176d8
Martin Jarábek <martin.jar91@seznam.cz>
2025-04-02 15:51:14 +0200

    Přidání struktury stránky s Tailwind CSS a základní navigací

--- main/views.py
+++ main/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'main/index.html')
+    context = {
+        'cv_link': 'odkaz_na_cv',
+        'leetcode_link': 'odkaz_na_leetcode',
+        'github_link': 'https://github.com/tvoje-jmeno',
+    }
+    return render(request, 'main/index.html', context)
