e8542d41083535fa3247f551911505dbe6855cab
Martin Jarábek <martin.jar91@seznam.cz>
2025-04-06 11:58:45 +0200

    Adding the leetcode-data.js file for the task list in the LeetCode section and modifying the script for functionality

---
+++ leetcode-data.js
@@ -0,0 +1,26 @@
+const leetcodeTasks = [
+    {
+      title: "Úloha 1: Two Sum",
+      description: "Popis úlohy: Najděte dvě čísla v poli, jejichž součet je roven dané hodnotě target. Vráťte indexy těchto čísel. Každý vstup má přesně jedno řešení a není dovoleno použít stejné číslo dvakrát.",
+      problemUrl: "https://leetcode.com/problems/two-sum/",
+      solutionUrl: "https://github.com/Matty-Jaris/LeetCode-solutions/blob/main/1_two_sum"
+    },
+    {
+      title: "Úloha 2: Palindrome Number",
+      description: "Popis úlohy: Úloha spočívá v tom, zjistit, zda je dané celé číslo palindrom. Palindrom je číslo, které čte stejně zleva i zprava. Úkolem je vrátit hodnotu true, pokud je číslo palindrom, nebo false, pokud není.",
+      problemUrl: "https://leetcode.com/problems/palindrome-number/",
+      solutionUrl: "https://github.com/Matty-Jaris/LeetCode-solutions/blob/main/2_palindrome_number"
+    },
+    {
+      title: "Úloha 3: Roman to Integer",
+      description: "Popis úlohy: Úloha spočívá v převodu římského čísla na celé číslo. Římská čísla jsou tvořena kombinací symbolů, přičemž některé symboly, jako například I před V nebo X, znamenají odečítání. Cílem je správně interpretovat tuto kombinaci a převést ji na odpovídající hodnotu celého čísla.",
+      problemUrl: "https://leetcode.com/problems/roman-to-integer/",
+      solutionUrl: "https://github.com/Matty-Jaris/LeetCode-solutions/blob/main/3_roman_to_integer"
+    },
+    {
+      title: "Úloha 4: Longest Common Prefix",
+      description: "Úloha spočívá v nalezení nejdelšího společného prefixu mezi řetězci v poli. Pokud takový prefix neexistuje, vrátí se prázdný řetězec.",
+      problemUrl: "https://leetcode.com/problems/longest-common-prefix/",
+      solutionUrl: "https://github.com/Matty-Jaris/LeetCode-solutions/blob/main/4_longest_common_prefix"
+    },
+  ];
