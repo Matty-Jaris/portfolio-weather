e7ec6782f3d8553210c35fac8dd08ed84bb0897c
Martin Jarábek <martin.jar91@seznam.cz>
2025-04-10 17:49:01 +0200

    Fix: correct combination of WSGI (Django) and FastAPI in ASGI router

--- asgi.py
+++ asgi.py
@@ -2,16 +2,17 @@ import os
 import django
 from fastapi import FastAPI
 from weather_app.main import app as fastapi_app
-from django.core.asgi import get_asgi_application
+from django.core.wsgi import get_wsgi_application
 from starlette.middleware.wsgi import WSGIMiddleware
 from starlette.routing import Mount, Router
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
 django.setup()
 
-# Kombinace Django + FastAPI
-django_app = get_asgi_application()
+# Django jako WSGI aplikace
+django_app = get_wsgi_application()
 
+# Kombinace Django + FastAPI
 application = Router([
     Mount("/", app=WSGIMiddleware(django_app)),
     Mount("/weather_app", app=fastapi_app),
